61d7e3c353de263cef589a6df11fb80b63f85e63
Andrew Loh <acloh2@illinois.edu>
2024-02-06 19:31:53 -0600

    Start of utils unleashed

--- utilities_unleashed/time.c
+++ utilities_unleashed/time.c
@@ -3,6 +3,13 @@
  * CS 341 - Spring 2024
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include "format.h"
+
 int main(int argc, char *argv[]) {
     return 0;
 }
