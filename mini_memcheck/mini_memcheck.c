a6a0e9f9152dbf057aff026a8eb1eea490215d08
Andrew Loh <acloh2@illinois.edu>
2024-02-13 21:53:36 -0600

    mini_memcheck p1

--- mini_memcheck/mini_memcheck.c
+++ mini_memcheck/mini_memcheck.c
@@ -51,14 +51,14 @@ void *mini_calloc(size_t num_elements, size_t element_size,
     size_t total_size = num_elements * element_size;
 
     // alloc with mini malloc
-    void *user_mem = mini_malloc(total_size, filename, intstruction);
+    void *user_mem = mini_malloc(total_size, filename, instruction);
     if (!user_mem) {
         perror("mini_calloc faield to allocate memory");
         return NULL;
     }
 
     // zero out memory
-    memset(user_mem, 0, total_size)
+    memset(user_mem, 0, total_size);
     return user_mem;
 }
 
@@ -68,9 +68,50 @@ void *mini_realloc(void *payload, size_t request_size, const char *filename,
     // May need to update head
     // update total_memory_requested
     // Check invalid_addresses
-    
+    if (!payload) {
+        // payload NULL, mini malloc
+        return mini_malloc(request_size, filename, instruction);
+    } else if (request_size == 0) {
+        // request size is 0, mini free
+        mini_free(payload);
+        return NULL;
+    }
 
-    return NULL;
+    // Find corresponding metadata for payload
+    meta_data *curr = head;
+    meta_data *prev = NULL;
+    // iterate and find
+    while (curr != NULL) {
+        if ((void *) (curr + 1) == payload) {
+            break; // found
+        }
+        // updating pointers
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if (!curr) {
+        // payload not found
+        invalid_addresses++;
+        return NULL;
+    }
+
+    // Allocate new mem with increased size
+    void *new_payload = mini_malloc(request_size, filename, instruction);
+    if (!new_payload) {
+        // failed to alloc
+        return NULL;
+    }
+
+    // copy old data to new memory
+    size_t old_size = curr->request_size;
+    // 3rd arg: size given is the min of the 2, truncate if new size is smaller
+    memcpy(new_payload, payload, old_size < request_size ? old_size : request_size);
+
+    //free
+    mini_free(payload);
+
+    return new_payload;
 }
 
 void mini_free(void *payload) {
@@ -78,4 +119,32 @@ void mini_free(void *payload) {
     // May need to update head
     // update total_memory_freed
     // invalid_addresses
+    if (!payload) {
+        // NULL, do nothing
+        return;
+    }
+
+    // Check payload in list
+    // pointer to pointer
+    meta_data **indirect_ptr = &head;
+    //iterate to end of list
+    while (*indirect_ptr != NULL) {
+        // point to metadata
+        meta_data *curr = *indirect_ptr;
+        // check curr payload matches payload to free
+        if ((void *) (curr + 1) == payload) {
+            // found, now remove
+            // update pointer links/ relinking with next node
+            *indirect_ptr = curr->next;
+            // update total free
+            total_memory_freed += curr->request_size;
+            // free meta data and payload
+            free(curr);
+            // exit to avoid double free
+            return;
+        }
+        indirect_ptr = &curr->next;
+    }
+    // not found
+    invalid_addresses++;
 }
