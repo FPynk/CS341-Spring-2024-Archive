9968a04d849372aaccc92d69c54c9b7fe75694cd
Andrew Loh <acloh2@illinois.edu>
2024-02-17 19:06:28 -0600

    Erased long comment

--- shell/shell.c
+++ shell/shell.c
@@ -477,68 +477,6 @@ int is_background_command(const char *cmd) {
     return *end == '&';
 }
 
-    // TODO: This is the entry point for your shell.
-    // Print a command prompt
-    // Read the command from standard input
-    // Print the PID of the process executing the command
-
-    // suport 2 optional arguments (use getopt)
-    // -h load history file as history, (STLL KEEP HISTORY if not called, just do not write to file)
-    // When exit, append commands of current session into history file
-    // -f run series of commands from script file
-    // print and run commands in sequential order til EOF
-    // if inc no of args or no script file, use format.h print and exit
-
-    // prompting for command: pid & path, no newline ->(pid=<pid>)<path>$
-    // reads from stdin or -f for file
-
-    // built-in and external
-    // built-in: exe w/o creating new process (No need to print PID)
-    // external: exe w/ creating new process (not one of built in listed)
-    // If run by new process, print PID: Command executed by pid=<pid>, see format.h
-    // Command arguments: whitespace delimited w/o trailing whitespace, no need to support quotes
-    // STORE IN HISTORY
-
-    // Exit:
-    // Receive exit from stdin
-    // EOF at beginning of the line (CTRL-D)
-    // or sent from script file
-    // If processes still running: KILL and cleanup children before exit
-    // ignore sigterm
-    // DO NOT STORE EXIT in history
-
-    // Ctrl-c / SIGINT catching
-    // Shell itself ignore SIGINT
-    // Kill currently running foreground proces
-    // Use KILL on foreground when SIGINT Caught
-    // Or: do nothing and let natural propagation, to kill only foreground process, use setgpid to assign new process group
-
-    // Built in commands
-    // cd <path>
-    // if no / then follow relative
-    // DNE then print error format.h, treated as nonexistent directory
-    // path arg mandatory
-    // use system call
-
-    // !history
-    // Prints out each command in the history in order
-    // DO NOT STORE THIS COMMAND IN HISTORY
-
-    // #<n>
-    // prints and exe n-th command of history, chrono order, earliest to most recent
-    // n non negative CHECK
-    // command exe store again in hist
-    // n not valid index prin appropriate error
-    // Print out command before exe if match
-    // #n itself not stored, just command it exe
-
-    // !<prefix>
-    // prints and executes LAST command that has specified prefix
-    // no match, print error, do not store in hist
-    // prefix may be empty
-    // print out cmd b4 exe if match
-    // do not store !prefix, store the command you exe
-
 int shell(int argc, char *argv[]) {
     debug_print("Function: shell");
     signal(SIGINT, catch_sigint);
