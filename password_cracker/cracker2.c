4fa69a98a5a5b0c97a14165e989527b8e38652e3
Andrew Loh <acloh2@illinois.edu>
2024-03-19 16:49:14 -0500

    need to rever commit for cracker 2

--- password_cracker/cracker2.c
+++ password_cracker/cracker2.c
@@ -18,6 +18,8 @@
 pthread_mutex_t mutex_global = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
+// REVERT THIS TO CRACKER 1 HASH COUNT FIXED
+
 // TODO:
     // TASK:
     // set mutex lock
