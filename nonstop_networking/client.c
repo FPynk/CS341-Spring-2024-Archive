4413fcaf53dace9c123569e99d627dde946d529e
Andrew Loh <acloh2@illinois.edu>
2024-04-13 23:45:05 -0500

    MP NN Skeleton

--- nonstop_networking/client.c
+++ nonstop_networking/client.c
@@ -11,38 +11,126 @@
 #include <unistd.h>
 #include <signal.h>
 
+// server imports
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+
 #include "common.h"
 
 char **parse_args(int argc, char **argv);
 verb check_args(char **args);
 
+// dynamically allocated addr_info
+static struct addrinfo *addr_structs;
+// File descriptor for server socket
+static volatile int serverSocket;
+
 // Helpers
-// Fn executes when SIGINT
-void SIGINT_handler(int signal) {
-    if (signal == SIGINT) {
-        fprintf(stderr, "SIGINT Handler: WIP\n");
+// free addr_structs
+void free_addr_info() {
+    if (addr_structs) {
+        freeaddrinfo(addr_structs);
+        addr_structs = NULL;
     }
 }
 
+/**
+ * Shuts down connection with 'serverSocket'.
+ * Called by close_program upon SIGINT.
+ */
+void close_server_connection() {
+    // Your code here
+    // Check addr info allocated, free if so
+    if (addr_structs) {
+        freeaddrinfo(addr_structs);
+        addr_structs = NULL;
+    }
+    // Shutdwon read part of socket to stop read ops
+    shutdown(serverSocket, SHUT_RD);
+    // close server socket
+    close(serverSocket);
+}
+
+
+/**
+ * Sets up a connection to a chatroom server and returns
+ * the file descriptor associated with the connection.
+ *
+ * host - Server to connect to.
+ * port - Port to connect to server on.
+ *
+ * Returns integer of valid file descriptor, or exit(1) on failure.
+ */
+int connect_to_server(const char *host, const char *port) {
+    // Q1-7
+    // Note: Need to use IPv4 TCP
+    struct addrinfo c_info;
+    // socket file descriptor for client
+    int sockfd;
+    // initialise c_info to zero and set desired properties
+    memset(&c_info, 0, sizeof(struct addrinfo));
+    c_info.ai_family = AF_INET; // IPv4
+    c_info.ai_socktype = SOCK_STREAM; // TCP
+    c_info.ai_protocol = 0;
+
+    // get list of addr structures for the server
+    int addr_res;
+    if ((addr_res = getaddrinfo(host, port, &c_info, &addr_structs)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(addr_res));
+        free_addr_info();
+        exit(EXIT_FAILURE);
+    }
+    // Create client socket
+    if ((sockfd = socket(c_info.ai_family, c_info.ai_socktype, c_info.ai_protocol)) < 0) {
+        perror("Cannot create client socket");
+        free_addr_info();
+        exit(EXIT_FAILURE);
+    }
+    // Attempt connection using the obtained addr structs
+    struct addrinfo *temp;
+    for (temp = addr_structs; temp != NULL; temp = temp->ai_next) {
+        if (connect(sockfd, temp->ai_addr, temp->ai_addrlen) != -1) {
+            // Successful connection, stop trying
+            break;
+        }
+    }
+
+    // Check connection successful
+    if (!temp) {
+        perror("Cannot connect to server");
+        free_addr_info();
+        exit(EXIT_FAILURE);
+    }
+
+    // Mem handle
+    free_addr_info();
+    return sockfd;
+}
+
+
 // Request functions for various methods
 // Handles GET method, return status 0 success -1 error
-int GET_request(remote, local) {
-
+int GET_request(const char *remote, const char *local) {
+    return 0;
 }
 
 // Handles PUT method, return status 0 success -1 error
-int PUT_request(remote, local) {
-
+int PUT_request(const char *remote, const char *local) {
+    return 0;
 }
 
 // Handles LIST method, return status 0 success -1 error
 int LIST_request() {
-
+    return 0;
 }
 
 // Handles DELETE method, return status 0 success -1 error
-int DELETE_request(remote) {
-
+int DELETE_request(const char *remote) {
+    return 0;
 }
 
 int main(int argc, char **argv) {
@@ -50,11 +138,9 @@ int main(int argc, char **argv) {
     check_args(argv);
     // char* array in form of {host, port, method, remote, local, NULL}
     char **args = parse_args(argc, argv); // delete
-    // signal handler
-    signal(SIGINT, SIGINT_handler);
 
     // Connect to server: args[0], args[1]
-    // WIP
+    serverSocket = connect_to_server(args[0], args[1]);
 
     // Check method
     // Store in local vars
@@ -77,11 +163,11 @@ int main(int argc, char **argv) {
         print_client_help();
         status = 1;
     }
-
+    fprintf(stderr, "status: %d\n", status);
     // Memory management
     free(args);
     // Close connection
-
+    close_server_connection();
     return 0;
 }
 
